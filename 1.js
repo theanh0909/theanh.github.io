7490f6e3502cbda2d5c6047efce067f62132e3f1
nguyentheanh <theanh@gxd.vn>
2019-02-01 23:44:24 +0700

    xu ly Jquery con chuot va cac menu

--- 1.js
+++ 1.js
@@ -1,4 +1,15 @@
 ﻿ $(function(){
- 
-})  
- 
+    $(".xuong, .kn").click(function(){
+        $('body,html').animate({scrollTop:$('#khoiedu').offset().top},600)
+    })
+
+    $(".sp").click(function(){
+        $('body,html').animate({scrollTop:$('#sanpham').offset().top},600)
+    })
+    $(".lh").click(function(){
+        $('body,html').animate({scrollTop:$('#lienhe').offset().top},600)
+    })
+    $(".cm").click(function(){
+        $('body,html').animate({scrollTop:$('#khoicomment').offset().top},600)
+    })
+})
